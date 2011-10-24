1b4a650d3d2b4db399f88b48b9a3f2aa156acc2d
Seth Snel <sethsnel@hotmail.com>
2011-10-24 16:02:49 +0200

    admin page added

---
+++ src/com/veilingsite/client/pages/AdminAuctionPage.java
@@ -0,0 +1,22 @@
+package com.veilingsite.client.pages;
+
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.veilingsite.client.widgets.EditCategoriesWidget;
+
+public class AdminAuctionPage extends VerticalPanel {
+	
+	private Label myLabel = new Label();
+	
+	private EditCategoriesWidget ecw = new EditCategoriesWidget();
+	private RootPanel containerLeft = RootPanel.get("containerLeft");
+	private RootPanel containerRight = RootPanel.get("containerRight");
+	
+	public AdminAuctionPage(){
+		add(myLabel);
+		
+		containerLeft.add(ecw);		
+	}
+
+}
