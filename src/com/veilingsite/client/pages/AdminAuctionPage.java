5eb168e9757147f9a80abd6f65cfc39b5d72ab5d
Jaap Koelewijn <jaaplallie@gmail.com>
2011-10-26 12:41:13 +0200

    plaatjes en aanpassingen aan adminusereditwidget met email check

--- src/com/veilingsite/client/pages/AdminAuctionPage.java
+++ src/com/veilingsite/client/pages/AdminAuctionPage.java
@@ -3,6 +3,7 @@ package com.veilingsite.client.pages;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.veilingsite.client.widgets.AdminUserEditWidget;
 import com.veilingsite.client.widgets.EditCategoriesWidget;
 
 public class AdminAuctionPage extends VerticalPanel {
@@ -10,13 +11,15 @@ public class AdminAuctionPage extends VerticalPanel {
 	private Label myLabel = new Label();
 	
 	private EditCategoriesWidget ecw = new EditCategoriesWidget();
+	private AdminUserEditWidget auew = new AdminUserEditWidget();
 	private RootPanel containerLeft = RootPanel.get("containerLeft");
 	private RootPanel containerRight = RootPanel.get("containerRight");
 	
 	public AdminAuctionPage(){
 		add(myLabel);
 		
-		containerLeft.add(ecw);		
+		containerLeft.add(ecw);
+		containerRight.add(auew);	
 	}
 
 }
